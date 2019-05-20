cc2087fe82f70abef9be1773001ab83d9efc0fc1
tapmorales <tapmorales@gmail.com>
2019-05-20 15:33:04 -0300

    onError and onListening

--- api/server.js
+++ api/server.js
@@ -13,6 +13,8 @@ const route = router.get('/', (req, res, next)=> {
 app.use(route)
    
 app.listen(port)
+app.on('error', onError)
+app.on('listening', onListening);
 
 
 function normalizePort(val) {
@@ -29,3 +31,35 @@ function normalizePort(val) {
     return false;
   }
 
+
+  function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }
