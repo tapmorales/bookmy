268ad9d80442ad52c9f004dfd233ce575647a4ed
tapmorales <tapmorales@gmail.com>
2019-05-22 15:14:18 -0300

    models and controllers for urls

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const config = require('../config')
 
 const router = express.Router()
 
+//carrega os models
+const Urls = require('./models/urls.model')
+const Tags = require('./models/tags.model')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+mongoose.connect(config.connectionString, { useNewUrlParser: true, useFindAndModify: true })
+//mongoose.set('useCreateIndex', true); //para poder usar unique no model
 
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
--- api/src/controllers/urls.controller.js
+++ api/src/controllers/urls.controller.js
@@ -1,30 +1,105 @@
-exports.get = (req, res, next)=>{
-    res.status(200).send({
-        "json": "lista de urls do usuario"
-    })
+const mongoose = require('mongoose')
+const Url = mongoose.model('Url')
+const ValidatorHttp = require('../helper/validateDataHttp')
+//const Url = require('../models/urls.model')
+
+exports.get = async (req, res, next)=>{
+    // res.status(200).send({
+    //     "json": "lista de urls do usuario"
+    // })
+    try{
+        let data = await Url.find({}, 'url tags title description private')
+        res.status(200).send(data)
+    } catch(e){
+        res.status(500).send({message: 'erro 500', erro: e})
+    }
+    
 }
 
-exports.post = (req, res, next)=>{
-    res.status(201).send(req.body)
+exports.post = async (req, res, next)=>{
+
+    let validate = new ValidatorHttp()
+    validate.isRequired(req.body.url, 'URL is required')
+    validate.isUnique(await Url.findOne({url: req.body.url}), 'A URL precisa ser única')
+
+    if(!validate.isValid()){
+        res.status(400).send(validate.errors).end()
+        return
+    }
+
+
+    const url = new Url()
+    url.url = req.body.url
+    url.tags = req.body.tags
+    url.title = req.body.title
+    url.description = req.body.description
+    url.private = req.body.private || false
+
+    
+    
+
+    try{
+        let data = await url.save()
+        res.status(201).send(data)
+    } catch(e){
+        res.status(400).send({'Erro': e})
+    }
 }
 
-exports.delete = (req, res, next)=>{
+exports.delete = async (req, res, next)=>{
     let _id = req.params.id
-    res.status(200).send({
-        "json": "lista de urls do usuario"
-    })
+    try{
+        await Url.findByIdAndRemove(_id)
+        res.status(200).send({
+            url: req.body.url,
+            tags: req.body.tags,
+            title: req.body.title,
+            description: req.body.description
+        })
+    } catch(e){
+        res.status(500).send({'Erro': e})
+    }
 }
 
-exports.put = (req, res, next)=>{
+exports.put = async (req, res, next)=>{
     let _id = req.params.id
-    res.status(200).send({
-        _id, 
-        "item": req.body        
-    })
+    let newUrl = {
+        url: req.body.url,
+        tags: req.body.tags,
+        title: req.body.title,
+        description: req.body.description,
+        private: req.body.private || false
+    }
+    try{
+        let data = await Url.findByIdAndUpdate(_id, {
+            $set: newUrl
+        })
+        //data são os dados antigos
+        res.status(200).send(newUrl)
+    } catch(e){
+        res.status(500).send({'Erro': e})
+    }
+    // res.status(200).send({
+    //     _id, 
+    //     "item": req.body        
+    // })
 }
 
-exports.getByTag = (req, res, next)=>{
-    res.status(200).send({
-        "json": "lista de urls do usuario"
-    })
+exports.getByTag = async (req, res, next)=>{
+
+    const _tags = (req.params.tags).split(',')
+    // res.status(200).send({
+    //     "json": "lista de urls do usuario"
+    // })
+    try{
+        let data = await Url.find({ 
+            tags: {
+                $all:  _tags
+            } 
+        })
+        res.status(200).send(data)
+    } catch(e){
+        res.status(500).send({'Erro': e})
+    }
+    
 }
